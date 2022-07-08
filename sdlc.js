e0145221b98ef21891628474d2525513fe389176
Federico Rocchetti <federico_rocchetti@hotmail.com>
2022-07-07 21:59:14 -0400

    Variables Completed

--- sdlc.js
+++ sdlc.js
@@ -17,14 +17,14 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
 
-const analysisOfRequirements = "The second step, it's where all requirements needed to achieve the end result are determined. This may include resources, existing features, new features"
+const analysisOfRequirements = "The second step, it's where all requirements needed to achieve the end result are determined. This may include resources, existing features, new features";
 
-const design = "The third step, where the design for the software being developed is laid out in order to achieve the end result. Everything from architecture, user interface, platforms where it will run, programming, security, data storage, etc. are included in this step."
+const design = "The third step, where the design for the software being developed is laid out in order to achieve the end result. Everything from architecture, user interface, platforms where it will run, programming, security, data storage, etc. are included in this step.";
 
-const implementation = "Once the design is completed this is the step where all components of the software design are coded and implemented."
+const implementation = "Once the design is completed this is the step where all components of the software design are coded and implemented.";
 
-const testingAndIntegration = "Once the implementation step is completed, the software developed is thorougly tested for any bugs and/or defects that may be present in order to get them corrected. Once the team is satisfied with the product after testing, the software is integrated (deployed) and made available to users."
+const testingAndIntegration = "Once the implementation step is completed, the software developed is thorougly tested for any bugs and/or defects that may be present in order to get them corrected. Once the team is satisfied with the product after testing, the software is integrated (deployed) and made available to users.";
 
-const maintain = "Once deployed, maintenance is crucial for tracking and fixing any bugs or glitches that may have been missed during testing, and to make sure it is working as intended."
+const maintain = "Once deployed, maintenance is crucial for tracking and fixing any bugs or glitches that may have been missed during testing, and to make sure it is working as intended.";
